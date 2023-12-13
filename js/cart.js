49f8b7f535000ad512ef4479b3d25688c990641a
Mamen <fbmaricarmen@gmail.com>
2023-12-13 14:50:04 +0100

    number cart

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-// Espera a que el DOM esté completamente cargado para ejecutar el código
+ Espera a que el DOM esté completamente cargado para ejecutar el código
 document.addEventListener('DOMContentLoaded', () => {
     // Script para cargar productos y manipular el DOM
     fetch('../js/data.json')
@@ -85,3 +85,83 @@ document.addEventListener('DOMContentLoaded', () => {
     // ... (código posterior)
   });
   
+
+  //nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn
+
+/*document.addEventListener('DOMContentLoaded', () => {
+  // Datos de productos (puedes cargarlos desde tu archivo JSON)
+  const productsData = [
+    { id: 1, name: 'Producto 1', price: 10, img: 'imagen1.jpg' },
+    { id: 2, name: 'Producto 2', price: 20, img: 'imagen2.jpg' },
+    // Agrega más productos según sea necesario
+  ];
+
+  const carrito = [];
+  const carritoLista = document.getElementById('carrito-lista');
+  const totalCarritoElement = document.getElementById('total-carrito');
+
+  // Función para mostrar el carrito en el DOM
+  function renderCarrito() {
+    carritoLista.innerHTML = '';
+    let totalCarrito = 0;
+
+    carrito.forEach(item => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${item.name} - ${item.quantity} x ${item.price}€`;
+      carritoLista.appendChild(listItem);
+
+      totalCarrito += item.quantity * item.price;
+    });
+
+    totalCarritoElement.textContent = totalCarrito;
+  }
+
+  // Función para agregar un producto al carrito
+  function addToCart(product) {
+    const existingItem = carrito.find(item => item.id === product.id);
+
+    if (existingItem) {
+      existingItem.quantity += 1;
+    } else {
+      carrito.push({ ...product, quantity: 1 });
+    }
+
+    renderCarrito();
+  }
+
+  // Cargar productos en las secciones correspondientes
+  const section1 = document.querySelector('#gluten');
+  const section2 = document.querySelector('#lacteo');
+  const section3 = document.querySelector('#frutos');
+
+  productsData.forEach(producto => {
+    const nuevoProducto = document.createElement('div');
+    nuevoProducto.classList.add('product');
+    nuevoProducto.innerHTML = `
+      <img src="${producto.img}" alt="Imagen de ${producto.name}">
+      <div class="product-txt">
+        <h3>${producto.name}</h3>
+        <p class="precio">${producto.price}€</p>
+        <div class="cantidad-controles">
+          <button class="restar-cantidad" data-id="${producto.id}">-</button>
+          <span class="cantidad">0</span>
+          <button class="sumar-cantidad" data-id="${producto.id}">+</button>
+        </div>
+        <a href="#" class="agregar-carrito btn-2" data-id="${producto.id}">Agregar</a>
+      </div>
+    `;
+
+    // Agregar evento de clic para agregar al carrito
+    const agregarCarritoBtn = nuevoProducto.querySelector('.agregar-carrito');
+    agregarCarritoBtn.addEventListener('click', () => addToCart(producto));
+
+    if (producto.gluten) {
+      section1.appendChild(nuevoProducto);
+    } else if (producto.lacteo) {
+      section2.appendChild(nuevoProducto);
+    } else if (producto.frutos) {
+      section3.appendChild(nuevoProducto);
+    }
+  });
+});
+*/
