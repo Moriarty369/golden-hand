846b31f4ea2479d853704ca2bfeaf43ca20cf421
FiorellaSF <afiosf@gmail.com>
2023-12-16 22:23:07 +0100

    Adding modal

--- js/cart.js
+++ js/cart.js
@@ -13,26 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const nuevoProducto = document.createElement('div');
         nuevoProducto.classList.add('product');
         nuevoProducto.innerHTML = `
-        <img href="#openModal" src="${producto.img}" alt="Imagen de ${producto.name}">
-        <div class="product-txt">
-          <h3>${producto.name}</h3>
-          <p class="precio">${producto.price}€</p>
-          <div class="cantidad-controles">
-            <button class="restar-cantidad" data-id="${producto.id}">-</button>
-            <span class="cantidad">0</span>
-            <button class="sumar-cantidad" data-id="${producto.id}">+</button>
+          <img id="open-modal" class="open-modal"  src="${producto.img}" alt="Imagen de ${producto.name}">
+          <div class="product-txt">
+            <h3>${producto.name}</h3>
+            <p class="precio">${producto.price}€</p>
+            <div class="cantidad-controles">
+              <button class="restar-cantidad" data-id="${producto.id}">-</button>
+              <span class="cantidad">0</span>
+              <button class="sumar-cantidad" data-id="${producto.id}">+</button>
+            </div>
+            <a href="#" class="agregar-carrito btn-2" data-id="${producto.id}">Agregar</a>
           </div>
-        </div>
-        <div class="wishlist-container">
-            <button class="wishlist-button" onclick="addToWishlist()">
-              <span class="wishlist-text" id="wishlist-text">Agregar a Favoritos</span>
-              <img class="heart-icon" src="../images/heart-solid.svg" alt="">
-            </button>
-          </div>
-          <div>
-          <a href="#" class="agregar-carrito btn-2" data-id="${producto.id}">Agregar</a>
-          </div>
-      `;
+        `;
 
         if (producto.category === "celiaco" ) {
           section1.appendChild(nuevoProducto);
@@ -82,3 +74,28 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
+//Modal
+
+const windowBackground = document.getElementById('window-background')
+const windowContainer = document.getElementById('window-container')
+const openModal = nuevoProducto.querySelector('.open-modal');
+const closeModal = document.getElementById('close-modal')
+
+
+// Hacer forEach para seleccionar cada producto
+// openModal.addEventListener('click', () => 
+//   windowBackground.style.display = 'flex'
+// );
+
+const closeWindow = () => {
+  windowContainer.classList.add('close')
+
+  setTimeout(() => {
+  windowContainer.classList.remove('close')
+  windowBackground.style.display = 'none' }, 1000);
+  }
+
+  closeModal.addEventListener('click',() => closeWindow())
+
+  window.addEventListener('click', e=> e.target == windowBackground && closeWindow())
