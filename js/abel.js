20b570870bb499d7abfbc921980184aff558c032
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2023-12-18 09:11:54 +0100

    improving map

--- js/abel.js
+++ js/abel.js
@@ -2,9 +2,9 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('parallax-map'), {
         center: { lat: 40.39680634551337, lng: -3.6995415288354443 },
         zoom: 18,
-        mapId: '47dafbb6434643c7'
+        mapId: 'YOUR-ID'
     });
-    
+
 
     const mapIcon = {
         url: "../images/golden-hands.png",
@@ -20,14 +20,14 @@ function initMap() {
         icon: mapIcon
     });
     console.log(mapIcon)
-    directionsService = new google.maps.DirectionsService();
-    directionsRenderer = new google.maps.DirectionsRenderer({
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer({
         map: map,
         panel: document.getElementById('directions-panel'),
         suppressMarkers: true // Oculta los marcadores predeterminados de Google Maps
     });
-    
-  // ...
+
+    // ...
 
     userLocationMarker = new google.maps.Marker({
         position: null, // Se actualizará con la ubicación del usuario
@@ -39,7 +39,7 @@ function initMap() {
         },
     });
 
-// ...
+    // ...
 
 
     // Obtiene la ubicación del usuario
@@ -100,71 +100,3 @@ function calculateRoute(travelMode) {
 }
 
 
-
-
-// <!--  MAPA FUNCIONAL V1-->
-{/* <script>
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('parallax-map'), {
-      center: { lat: 40.39680634551337, lng: -3.6995415288354443 },
-      zoom: 18,
-      mapId: '47dafbb6434643c7'
-    });
-
-    const mapIcon = {
-      url: "./images/golden-hands.png",
-      scaledSize: new google.maps.Size(30, 38)
-    };
-
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-    directionsRenderer.setMap(map);
-
-    // Obtiene la ubicación del usuario
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const userLocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-
-          // Crea un marcador para la ubicación del usuario
-          new google.maps.Marker({
-            position: userLocation,
-            map,
-            title: "Tu ubicación",
-          });
-
-          // Calcula y muestra las indicaciones para llegar a tu tienda
-          const request = {
-            origin: userLocation,
-            destination: { lat: 40.39680634551337, lng: -3.6995415288354443 }, // Cambia por las coordenadas de tu tienda
-            travelMode: google.maps.TravelMode.DRIVING
-          };
-
-          directionsService.route(request, (response, status) => {
-            if (status === google.maps.DirectionsStatus.OK) {
-              directionsRenderer.setDirections(response);
-            } else {
-              console.error("Error al calcular la ruta:", status);
-            }
-          });
-        },
-        (error) => {
-          console.error("Error al obtener la ubicación del usuario:", error);
-        }
-      );
-    } else {
-      console.error("La geolocalización no está soportada por el navegador.");
-    }
-
-    // Crea un marcador para tu tienda
-    new google.maps.Marker({
-      position: { lat: 40.39680634551337, lng: -3.6995415288354443 },
-      map,
-      title: "Visítanos!",
-      icon: mapIcon
-    });
-  }
-</script> */}
