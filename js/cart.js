646e771eac21b29a7cfd2fa53e763129a56e6116
JeanCarlosPerez <jeancperez09@gmail.com>
2023-12-20 11:59:28 +0100

    changes

--- js/cart.js
+++ js/cart.js
@@ -1,115 +1,246 @@
-// Espera a que el DOM esté completamente cargado para ejecutar el código
+const carrito = new Map();
+let cantidadProductos = 0; // Inicializar el contador
+const contadorProductos = document.getElementById('contador-productos');
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    // Script para cargar productos y manipular el DOM
-    fetch('../js/data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Seleccionar las secciones donde se mostrarán los productos
-        const section1 = document.querySelector('#gluten');
-        const section2 = document.querySelector('#lacteo');
-        const section3 = document.querySelector('#frutos');
-  
-        // Iterar sobre cada producto en los datos
-        data.forEach(producto => {
-          // Crear un nuevo elemento div para representar el producto
-          const nuevoProducto = document.createElement('div');
-          nuevoProducto.classList.add('product');
-          nuevoProducto.innerHTML = `
-            <img href="#openModal" src="${producto.img}" alt="Imagen de ${producto.name}">
-            <div class="product-txt">
-              <h3>${producto.name}</h3>
-              <p class="precio">${producto.price}€</p>
-              <div class="cantidad-controles">
-                <button class="restar-cantidad" data-id="${producto.id}">-</button>
-                <span class="cantidad">0</span>
-                <button class="sumar-cantidad" data-id="${producto.id}">+</button>
-              </div>
-              <a href="#" class="agregar-carrito btn-2" data-id="${producto.id}">Agregar</a>
-            </div>
-          `;
+  let section1, section2, section3;
 
-  
-          // Asignar el producto a la sección correspondiente según su categoría
-          if (producto.category === "celiaco" ) {
-            section1.appendChild(nuevoProducto);
-          } else if (producto.category === "lacteos" )  {
-            section2.appendChild(nuevoProducto);
-          } else {
-            section3.appendChild(nuevoProducto);
-          }
-  
-          // Obtener referencias a elementos relevantes dentro del nuevo producto
-          const cantidadSpan = nuevoProducto.querySelector('.cantidad');
-          const restarBtn = nuevoProducto.querySelector('.restar-cantidad');
-          const sumarBtn = nuevoProducto.querySelector('.sumar-cantidad');
-          const agregarBtn = nuevoProducto.querySelector('.agregar-carrito');
-  
-          // Variable para rastrear la cantidad de productos seleccionada
-          let cantidad = 0;
-  
-          // Event listener para reducir la cantidad al hacer clic en el botón de restar
-          restarBtn.addEventListener('click', () => {
-            if (cantidad > 0) {
-              cantidad -= 1;
-              cantidadSpan.textContent = cantidad;
-            }
-          });
-  
-          // Event listener para aumentar la cantidad al hacer clic en el botón de sumar
-          sumarBtn.addEventListener('click', () => {
-            cantidad += 1;
-            cantidadSpan.textContent = cantidad;
-          });
-  
-          // Event listener para agregar el producto al carrito al hacer clic en el botón de agregar
-          agregarBtn.addEventListener('click', (event) => {
-            event.preventDefault();
-  
-            // Verificar que la cantidad seleccionada sea mayor que 0
-            if (cantidad > 0) {
-              // Buscar el producto en la lista de productos
-              const productoEncontrado = data.find(item => item.id === producto.id);
-  
-              // Agregar el producto al carrito
-              if (productoEncontrado) {
-                carrito.agregarItem(productoEncontrado, cantidad);
-              }
-            }
-          });
+  fetch('../js/data.json')
+    .then(response => response.json())
+    .then(data => {
+      section1 = document.querySelector('#gluten');
+      section2 = document.querySelector('#lacteo');
+      section3 = document.querySelector('#frutos');
+      const btnCeliaco = document.querySelector("#btnCeliaco")
+      const btnLacteos = document.querySelector("#btnLacteos")
+      const btnFrutos = document.querySelector("#btnFrutos")
+      const verTodo = document.querySelector("#verTodo")
+      const tituloCeliaco = document.querySelector("#tituloCeliaco")
+      const tituloLacteos = document.querySelector("#tituloLacteo")
+      const tituloFrutos = document.querySelector("#tituloFrutos")
+
+    verTodo.addEventListener('click', () => {
+      if (section1.style.display === 'none') {
+        tituloCeliaco.style.display = 'none'
+        tituloLacteos.style.display = 'none'
+        tituloFrutos.style.display = 'none'
+        section1.style.display = 'flex';
+        section2.style.display = 'flex';
+        section3.style.display = 'flex';
+      } else {
+        tituloCeliaco.style.display = 'none'
+        tituloLacteos.style.display = 'none'
+        tituloFrutos.style.display = 'none'
+        section1.style.display = 'flex';
+        section2.style.display = 'flex';
+        section3.style.display = 'flex';
+      }
+      const productosPorPagina = 3;
+      let paginaActual = 1;
+
+      function mostrarProductos() {
+        const inicio = (paginaActual - 1) * productosPorPagina;
+        const fin = inicio + productosPorPagina;
+        const productosPagina = data.slice(inicio, fin);
+
+        // Mostrar los productos en la página
+        const productosContainer = document.querySelector('.productos');
+        productosContainer.innerHTML = '';
+        productosPagina.forEach(producto => {
+          // Aquí debes mostrar cada producto en la página
         });
-      })
-      .catch(error => {
-        // Manejar errores en caso de que la carga de productos falle
-        console.error('Se ha producido un error al obtener los datos del archivo JSON', error);
+      }
+
+
+      // Mostrar los productos en la página al cargar
+      mostrarProductos();
       });
-  
-    // ... (código posterior)
-  });
 
+      btnCeliaco.addEventListener('click', () => {
+        if (section1.style.display === 'none') {
+          tituloCeliaco.style.display = 'flex'
+          tituloLacteos.style.display = 'none'
+          tituloFrutos.style.display = 'none'
+          section1.style.display = 'flex';
+          section2.style.display = 'none';
+          section3.style.display = 'none';
+        } else {
+          tituloCeliaco.style.display = 'flex'
+          tituloLacteos.style.display = 'none'
+          tituloFrutos.style.display = 'none'
+          section1.style.display = 'flex';
+          section2.style.display = 'none';
+          section3.style.display = 'none';
+        }
+      });
 
-  // Añadir al carrito
-  document.addEventListener('DOMContentLoaded', () => {
-    // ...
+      btnLacteos.addEventListener('click', () => {
+        if (section2.style.display === 'none') {
+          tituloCeliaco.style.display = 'none'
+          tituloLacteos.style.display = 'flex'
+          tituloFrutos.style.display = 'none'
+          section1.style.display = 'none';
+          section2.style.display = 'flex';
+          section3.style.display = 'none';
+        } else {
+          tituloCeliaco.style.display = 'none'
+          tituloLacteos.style.display = 'flex'
+          tituloFrutos.style.display = 'none'
+          section1.style.display = 'none';
+          section2.style.display = 'flex';
+          section3.style.display = 'none';
+        }
+      });
 
-    // Event listener para agregar el producto al carrito al hacer clic en el botón de agregar
-    agregarBtn.addEventListener('click', (event) => {
-        event.preventDefault();
+      btnFrutos.addEventListener('click', () => {
+        if (section3.style.display === 'none') {
+          tituloCeliaco.style.display = 'none'
+          tituloLacteos.style.display = 'none'
+          tituloFrutos.style.display = 'flex'
+          section1.style.display = 'none';
+          section2.style.display = 'none';
+          section3.style.display = 'flex';
+        } else {
+          tituloCeliaco.style.display = 'none'
+          tituloLacteos.style.display = 'none'
+          tituloFrutos.style.display = 'flex'
+          section1.style.display = 'none';
+          section2.style.display = 'none';
+          section3.style.display = 'flex';
+        }
+      });
 
-        // Verificar que la cantidad seleccionada sea mayor que 0
-        if (cantidad > 0) {
-            // Buscar el producto en la lista de productos
+      data.forEach(producto => {
+        const nuevoProducto = document.createElement('div');
+        nuevoProducto.classList.add('product');
+        nuevoProducto.innerHTML = `
+        <a id="open-modal" href="#"><img  src="${producto.img}" alt="Imagen de ${producto.name}"></a>
+        <div class="product-txt">
+          <h3>${producto.name}</h3>
+          <p class="precio">${producto.price}€</p>
+          <div class="cantidad-controles">
+            <button class="restar-cantidad" data-id="${producto.id}">-</button>
+            <span class="cantidad">0</span>
+            <button class="sumar-cantidad" data-id="${producto.id}">+</button>
+          </div>
+        </div>
+        <div class="wishlist-container">
+          <button class="wishlist-button" onclick="addToWishlist()">
+            <span class="wishlist-text" id="wishlist-text">Agregar a Favoritos</span>
+            <img class="heart-icon" src="../images/heart-solid.svg" alt="">
+          </button>
+        </div>
+        <div>
+          <a href="#" class="agregar-carrito btn-2" data-id="${producto.id}">Agregar</a>
+        </div>
+      `;
+
+        if (producto.category === "celiaco") {
+          section1.appendChild(nuevoProducto);
+          section1.style.display = 'flex'
+        } else if (producto.category === "lacteos") {
+          section2.appendChild(nuevoProducto);
+          section2.style.display = 'flex'
+        } else {
+          section3.appendChild(nuevoProducto);
+          section3.style.display = 'flex'
+        }
+
+        const cantidadSpan = nuevoProducto.querySelector('.cantidad');
+        const restarBtn = nuevoProducto.querySelector('.restar-cantidad');
+        const sumarBtn = nuevoProducto.querySelector('.sumar-cantidad');
+        const agregarBtn = nuevoProducto.querySelector('.agregar-carrito');
+
+        let cantidad = 0;
+        restarBtn.addEventListener('click', () => {
+          if (cantidad > 0) {
+            cantidad -= 1;
+            // Actualizar el contador de productos
+            actualizarContadorProductos();
+          }
+        });
+
+        sumarBtn.addEventListener('click', () => {
+          cantidad += 1;
+          cantidadSpan.textContent = cantidad;
+          // Actualizar el contador de productos
+          actualizarContadorProductos();
+        });
+
+        agregarBtn.addEventListener('click', (event) => {
+          event.preventDefault();
+
+          if (cantidad > 0) {
             const productoEncontrado = data.find(item => item.id === producto.id);
 
-            // Agregar el producto al carrito
             if (productoEncontrado) {
-                carrito.agregarItem(productoEncontrado, cantidad);
-
-                // Actualizar el carrito en localStorage
-                localStorage.setItem('carrito', JSON.stringify(carrito.getItems()));
+              carrito.set(productoEncontrado.id, { producto: productoEncontrado, cantidad });
+              // Actualizar localStorage
+              localStorage.setItem('carrito', JSON.stringify(Array.from(carrito.entries())));
+              console.log(carrito);
+              // Reiniciar la cantidad después de agregar al carrito
+              cantidad = 0;
+              cantidadSpan.textContent = cantidad;
+              // Actualizar el contador de productos
+              cantidadProductos += 1;
+              actualizarContadorProductos();
             }
-        }
-    });
-});
 
-  
-  
+          }
+        });
+      });
+     
+      
+      // agregando cantidad seleccionada al icono de la cesta
+
+      const btnContador = document.querySelectorAll('.agregar-carrito');
+
+      contadorProductos.textContent = cantidadProductos;
+      console.log(contadorProductos);
+       function actualizarContadorProductos() {
+        contadorProductos.textContent = cantidadProductos;
+        // Actualizar localStorage con el nuevo estado del carrito
+        localStorage.setItem('carrito', JSON.stringify(Array.from(carrito.entries())));
+      }
+      // Agregar un event listener a cada botón para escuchar los clics
+      btnContador.forEach(boton => {
+        boton.addEventListener('click', () => {
+          // Obtener el valor asociado al botón
+          const cantidadSeleccionada = parseInt(boton.dataset.cantidad, 10);
+
+          console.log(cantidadSeleccionada);
+          // Sumar la cantidad seleccionada a la cantidad total de productos
+          cantidadProductos += cantidadSeleccionada;
+
+          // Actualizar el contador con la nueva cantidad
+          contadorProductos.textContent = cantidadProductos;
+        });
+      });
+    })
+    .catch(error => {
+      console.error('Se ha producido un error al obtener los datos del archivo JSON', error);
+    });
+
+ 
+  //// MODAL 
+// Obtener todas las imágenes con la clase "open-modal"
+var images = document.querySelectorAll('.product img#open-modal');
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+images.forEach(img => {
+  img.onclick = function(){
+    var imageSrc = this.src;
+    var productName = this.alt.replace('Imagen de ', '');
+    var productPrice = this.nextElementSibling.querySelector('.precio').textContent;
+
+    modal.style.display = "block";
+    modalImg.src = imageSrc;
+    captionText.innerHTML = `<h3>${productName}</h3><p>${productPrice}</p>`;
+  }
+});
+///////// FIN MODAL
+
+});
